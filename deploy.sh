77d1a7c2213738e45265f280274d877a5cec011d
nooldey <openzws@163.com>
2018-04-26 19:14:58 +0800

    renew

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 # @Author: nooldey
 # @Date:   2018-04-26 17:48:46
 # @Last Modified by:   nooldey
-# @Last Modified time: 2018-04-26 18:00:16
+# @Last Modified time: 2018-04-26 19:13:29
 
 set -e
 
@@ -14,9 +14,10 @@ cd ./public
 # echo 'zhuweisheng.com.cn' > CNAME
 
 git init
-git remote add origin git@github.com:nooldey/bookmarks.git
+git remote add origin https://github.com/nooldey/bookmarks.git
 git add -A
 git commit -m 'auto deploy'
-git push -f git@github.com:nooldey/bookmarks.git master:gh-pages
+git push --set-upstream origin gh-pages
+git push -f https://github.com/nooldey/bookmarks.git master:gh-pages
 
 cd -
