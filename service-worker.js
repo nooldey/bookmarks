1a20f05c6f9e72c73effcc217d044d39a717a93f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-06-13 06:35:40 +0000

    Deploy nooldey/bookmarks to github.com/nooldey/bookmarks.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -21,10 +21,10 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "fac6a07914282bcccf43ebbea65fb2f4"
+    "revision": "816f336e833ac5a3e667dfd4231c2291"
   },
   {
-    "url": "assets/css/10.styles.60bbff25.css",
+    "url": "assets/css/11.styles.e6c6433b.css",
     "revision": "24eaea6d5e6d82ea9e22100e49519c08"
   },
   {
@@ -32,48 +32,52 @@ self.__precacheManifest = [
     "revision": "83621669651b9a3d4bf64d1a670ad856"
   },
   {
-    "url": "assets/js/0.e5711f0f.js",
-    "revision": "12de52c0094e4a96a7e27b9d861c2fee"
+    "url": "assets/js/0.7707d958.js",
+    "revision": "2237fc12707a5781dfdd729735900f3d"
   },
   {
-    "url": "assets/js/1.91b9dd99.js",
-    "revision": "c35622b435090b8511a5c6917f174409"
+    "url": "assets/js/1.f9175ed3.js",
+    "revision": "61d04d94c9eac0e4e62bdb3d9ec3a05d"
   },
   {
-    "url": "assets/js/2.03b52323.js",
-    "revision": "52d9ce93832fd570c940667a20858887"
+    "url": "assets/js/10.11f76bff.js",
+    "revision": "4ae918355c720df6218c7ec89623a374"
   },
   {
-    "url": "assets/js/3.bb73b757.js",
-    "revision": "220acd87216dacbb97c09c4a3f4cef16"
+    "url": "assets/js/2.8082e709.js",
+    "revision": "73d8a990fded885ae5989565b9b26d04"
   },
   {
-    "url": "assets/js/4.8971b08a.js",
-    "revision": "50145b3b5ead0b8d2d43f3ca8f220060"
+    "url": "assets/js/3.bfc8c3ca.js",
+    "revision": "65e8b88adef54199cc44389fd52c4552"
   },
   {
-    "url": "assets/js/5.8252a513.js",
-    "revision": "026fbd8785b48f8ca8daa2560fcd888a"
+    "url": "assets/js/4.af51e57f.js",
+    "revision": "53abf542efcb7419a296365275f99b7f"
   },
   {
-    "url": "assets/js/6.e1f69adf.js",
-    "revision": "c73c23ff6904a0e6fb76087e282fc521"
+    "url": "assets/js/5.91c4d265.js",
+    "revision": "787c11a933b59b1150551ae087fd9068"
   },
   {
-    "url": "assets/js/7.cc1e9fe0.js",
-    "revision": "5334b06a6da32146b5501d7bd2b54c88"
+    "url": "assets/js/6.c61b626f.js",
+    "revision": "f2a006152d9ae8a7302891a00b62e053"
   },
   {
-    "url": "assets/js/8.dcdeca5d.js",
-    "revision": "909039204d2584221336ead9edaaab6f"
+    "url": "assets/js/7.b7283216.js",
+    "revision": "d166dcec59d6b1e1e4e6adc2ddc6dd57"
   },
   {
-    "url": "assets/js/9.91d61d23.js",
-    "revision": "a6ee0e56e05c366f8582d23bbbebd446"
+    "url": "assets/js/8.b0bd694f.js",
+    "revision": "28c174de5475c5ac447c64b5cf676aff"
   },
   {
-    "url": "assets/js/app.4496553d.js",
-    "revision": "d1a0571102b27889b01a59ed2c66cc11"
+    "url": "assets/js/9.0a93e728.js",
+    "revision": "b4bf04e93428ae9e11e1993f94801f0a"
+  },
+  {
+    "url": "assets/js/app.af81d1f6.js",
+    "revision": "cf9f5dd0403e1f7f493d3f37162a1d98"
   },
   {
     "url": "avatar.png",
@@ -81,43 +85,47 @@ self.__precacheManifest = [
   },
   {
     "url": "chengxukaifaCoder/index.html",
-    "revision": "4b2ca85f4e4aa1a9f31150fd3f700e14"
+    "revision": "27f59896bf778928bdffe240093c8155"
   },
   {
     "url": "developer/index.html",
-    "revision": "bb4df7f9c57daff94a0491bb083429ec"
+    "revision": "7ba399797867718eee783001a7729f38"
   },
   {
     "url": "developer/vps.html",
-    "revision": "c05a54d4341abfc70a62cdaa5f474d1a"
+    "revision": "e56b3f7b964a2d6e1faaa3027241e0a8"
   },
   {
     "url": "dianshangyunying/index.html",
-    "revision": "6b20ed16a20db951080391518996db73"
+    "revision": "e171ab953115a72a7d520de914efea00"
   },
   {
     "url": "dianzishu/index.html",
-    "revision": "c36b417257eca323ab34d4cdd686332b"
+    "revision": "9a9f72f87263553ed5cf92be3f3ad981"
+  },
+  {
+    "url": "friends/accounts.html",
+    "revision": "a371e1b074f4d2ed3b7c313fa4fc168f"
   },
   {
     "url": "friends/index.html",
-    "revision": "daa40e869604b815fc6bd8b2bb48a998"
+    "revision": "0398b3a6e4bcc1657563180a4c57b3fb"
   },
   {
     "url": "index.html",
-    "revision": "ea75b6611fbf2246753efabd943e66ae"
+    "revision": "734e0675cbe62c581cb3bb4656d6eb1e"
   },
   {
     "url": "qianduanzaixiangongju/index.html",
-    "revision": "7fcacc860c1481fd5c1c4f850e94e585"
+    "revision": "5315b41b1e35a23cca522dbe2106dcf3"
   },
   {
     "url": "shoucangdewenzhang/index.html",
-    "revision": "5c5a1e91e1ab67268fb49c5d35a2d7c2"
+    "revision": "a5ce7afd50c76104c8e3a11bc609db2f"
   },
   {
     "url": "sousuoyinqing/index.html",
-    "revision": "d810d418c688e31a9d89561374f276a9"
+    "revision": "e0972f945256c15c995f8657d3acb600"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
