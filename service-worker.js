da968af1a30fc41ed52258eab5dd185cd35c969d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-06 06:03:16 +0000

    Deploy nooldey/bookmarks to github.com/nooldey/bookmarks.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -21,7 +21,7 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "aa12a2dc8c396c2c863c2abab5f2d75a"
+    "revision": "25d284e96b39f44816c55e55daec1548"
   },
   {
     "url": "assets/css/10.styles.60bbff25.css",
